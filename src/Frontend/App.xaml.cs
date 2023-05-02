03cf7775d7dce55e1cb22f6b81d05c20f3b513a1
Parham Saremi <parhaamsaremi@gmail.com>
2023-05-02 11:57:09 +0430

    first commit

---
+++ src/Frontend/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Frontend;
+
+public partial class App : Application
+{
+    internal static string DefaultDateTimeFormat = "dddd, dd/MMM/yyyy HH:mm";
+    public App()
+    {
+        InitializeComponent();
+
+        Page mainPage = new EventPage();
+        MainPage = new NavigationPage(mainPage);
+    }
+
+}
---
+++ src/Frontend/EventPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Frontend;
+
+public partial class EventPage : ContentPage
+{
+    public EventPage()
+    {
+        InitializeComponent();
+    }
+
+}
